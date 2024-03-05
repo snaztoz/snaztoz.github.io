0428862eb93ac03b96cacccf9e393507b31d897b
snaztoz <hafidhmn@gmail.com>
2024-03-05 14:17:24 +0700

    feat: :sparkles: add google analytics ID and change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,11 @@ module.exports = {
     './layouts/**/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Nunito Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
